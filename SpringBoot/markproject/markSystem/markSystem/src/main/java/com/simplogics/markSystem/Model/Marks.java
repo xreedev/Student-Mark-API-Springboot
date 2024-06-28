33ad763f92f59ba85fad1fda8a2e77fbf5b99ca5
sreedev <xreedev@gmail.com>
2024-06-28 11:54:20 +0530

    removed unwanted code

--- SpringBoot/markproject/markSystem/markSystem/src/main/java/com/simplogics/markSystem/Model/Marks.java
+++ SpringBoot/markproject/markSystem/markSystem/src/main/java/com/simplogics/markSystem/Model/Marks.java
@@ -27,35 +27,4 @@ public class Marks {
     @JoinColumn(name = "subId", nullable = false)
     private Subject subject;
 
-    public Integer getMarkId() {
-        return markId;
-    }
-
-    public void setMarkId(Integer markId) {
-        this.markId = markId;
-    }
-
-    public Integer getMark() {
-        return mark;
-    }
-
-    public void setMark(Integer mark) {
-        this.mark = mark;
-    }
-
-    public Student getStudent() {
-        return student;
-    }
-
-    public void setStudent(Student student) {
-        this.student = student;
-    }
-
-    public Subject getSubject() {
-        return subject;
-    }
-
-    public void setSubject(Subject subject) {
-        this.subject = subject;
-    }
 }
