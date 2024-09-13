9d13ccd8d1a34aa203338a2d740fd2a43448eb6c
sreedev <xreedev@gmail.com>
2024-09-13 23:41:12 +0530

    restructure

--- SpringBoot/SchoolSystem/SchoolSystem/src/main/java/com/simplogics/SchoolSystem/Repository/IStudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.simplogics.SchoolSystem.Repository;
-
-import com.simplogics.SchoolSystem.Model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IStudentRepository extends JpaRepository<Student,Integer> {
-}
--- SpringBoot/SchoolSystem/SchoolSystem/src/main/java/com/simplogics/SchoolSystem/Repository/ITeacherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.simplogics.SchoolSystem.Repository;
-
-import com.simplogics.SchoolSystem.Model.Teacher;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ITeacherRepository extends JpaRepository<Teacher,Integer> {
-}
